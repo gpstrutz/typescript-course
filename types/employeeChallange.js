cedbe5547c114250142885b4e0abd0d2c7c61c48
gpstrutz <giovannistrutz97@gmail.com>
2021-01-23 19:01:50 -0300

    Add the files about the 2 course challanges and your respectives answers

---
+++ types/employeeChallange.js
@@ -0,0 +1,42 @@
+"use strict";
+// Challange - Employee
+/*
+    Criar um objeto funcionario com:
+        - Array de strings com nome dos supervisores;
+        - Função de bater ponto que recebe a hora (numero)
+        e retorna uma string
+            -> Ponto Normal ( <= 8 )
+            -> Fora do Horário ( > 8 )
+*/
+// My Answer
+var employee = {
+    supervisors: ["Giovanni", "Beatriz", "Rafaela"],
+    hourRegister: function (hour) {
+        if (hour <= 8) {
+            return "Ponto Normal";
+        }
+        else {
+            return "Fora do Horário";
+        }
+    }
+};
+console.log(employee.supervisors);
+console.log(employee.hourRegister(8));
+console.log(employee.hourRegister(10));
+// Teacher's Answer
+/*
+
+let employee: { supervisors: string[], hourRegister: (hour: number) => string } = {
+    
+    supervisors: ["Giovanni", "Beatriz", "Ana"],
+
+    hourRegister(hour: number): string {
+        if (hour <= 8 ) {
+            return "Ponto Normal"
+        } else {
+            return "Fora Horário"
+        }
+    }
+}
+
+*/
